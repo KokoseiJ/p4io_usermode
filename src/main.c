fec630fdf18f0f72ef627361a8c79d58bf26ba26
Wonjun Jung <kokoseij@gmail.com>
2024-09-26 18:08:21 -0700

    Add basic bulk transfer / interrupt transfer checking

--- src/main.c
+++ src/main.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <string.h>
+#include <time.h>
 #include <libusb.h>
 
 //#define VID 0x0b05
@@ -8,6 +10,13 @@
 
 #define INTERRUPT_SIZE 0x10
 #define BULK_SIZE 64
+#define LIGHTS_SIZE 0x10
+#define INT_SIZE 0x10
+
+#define P4IO_CMD_LIGHTS 0x12
+
+#define USB_FRAME 1000
+#define USB_NANOFRAME 125
 
 /*
 LIBUSB_ENDPOINT_TRANSFER_TYPE_CONTROL
@@ -24,13 +33,16 @@ Interrupt endpoint.
 */
 
 int main() {
-    int rtnval, i, j, k;
+    int rtnval, i, j, transferred, interval_usec, int_buffer[4];
     libusb_device *dev = NULL;
     libusb_device_handle *dev_handle = NULL;
     struct libusb_config_descriptor *config = NULL;
-    struct libusb_interface *interface = NULL;
-    struct libusb_interface_descriptor *iface_desc = NULL;
-    struct libusb_endpoint_descriptor *endpoint = NULL;
+    struct libusb_interface_descriptor *iface = NULL;
+    struct libusb_endpoint_descriptor *bulk_out, *bulk_in, *intr_in;
+
+    struct timespec t = {1, 0};
+
+    uint8_t lights_buffer[LIGHTS_SIZE] = {0,}, intr_interval, speed;
 
     libusb_init_context(NULL, NULL, 0);
 
@@ -41,41 +53,83 @@ int main() {
     }
 
     dev = libusb_get_device(dev_handle);
-    libusb_get_config_descriptor(dev, 0, &config); // to be freed
-    printf("[*] config->bNumInterfaces: %hhu\n", config->bNumInterfaces);
-    // TODO: drier sends first config desc back again, do I do that here?
-    
-    for (i=0; i<config->bNumInterfaces; i++) {
-        interface = &config->interface[i];
-        printf("[*] interface[%d]->num_altsetting: %d\n", i, interface->num_altsetting);
-        for (j=0; j<interface->num_altsetting; j++) {
-            iface_desc = &interface->altsetting[j];
-            printf("[*] iface[%d]->bNumEndPoints: %hhu\n", j, iface_desc->bNumEndpoints);
 
-            for (k=0; k<iface_desc->bNumEndpoints; k++) {
-                endpoint = &iface_desc->endpoint[k];
-                printf("[*] endpoint[%d] addr:0x%02hhx %s\n", k, endpoint->bEndpointAddress, endpoint->bEndpointAddress>>7 == 1 ? "IN" : "OUT");
-                printf("[*] wMaxPacketSize: %hu\n", endpoint->wMaxPacketSize);
-                printf("[*] transfer type: ");
-                switch (endpoint->bmAttributes & 3) {
-                    case 0:
-                    printf("Control");
-                    break;
-                    case 1:
-                    printf("Isochronous");
-                    break;
-                    case 2:
-                    printf("Bulk");
-                    break;
-                    case 3:
-                    printf("Interrupt");
-                    break;
-                }
-                printf("\n\n");
+    speed = libusb_get_device_speed(dev);
+
+    printf("[*] Device speed is: ");
+    switch (speed) {
+        case LIBUSB_SPEED_UNKNOWN:
+        printf("sorrgy accident");
+        break;
+        case LIBUSB_SPEED_LOW:
+        printf("yo this shit slow af");
+        break;
+        case LIBUSB_SPEED_FULL:
+        printf("Full Speed");
+        break;
+        case LIBUSB_SPEED_HIGH:
+        printf("im going fast as fuck boi");
+        break;
+        case LIBUSB_SPEED_SUPER:
+        printf("eminem");
+        break;
+        case LIBUSB_SPEED_SUPER_PLUS:
+        printf("FUUUUUTURE   FUUUUUUUUUUTUUUUREEEEEE");
+        break;
+    }
+    printf("\n");
+
+    libusb_get_config_descriptor(dev, 0, &config); // to be freed
+    iface = &config->interface->altsetting[0];
+    bulk_out = &iface->endpoint[0];
+    bulk_in = &iface->endpoint[1];
+    intr_in = &iface->endpoint[2];
+
+    printf("[*] Starting lights test\n\n");
+
+    for (i=0; i<LIGHTS_SIZE; i++) {
+        memset(lights_buffer, 0, LIGHTS_SIZE);
+        lights_buffer[i] = 255;
+        transferred = 0;
+
+        printf("[*] Lights %d\n", i);
+
+        rtnval = libusb_bulk_transfer(dev_handle, bulk_out->bEndpointAddress, lights_buffer, LIGHTS_SIZE, &transferred, 0);
+        
+        printf("rtnval: %d transferred: %d\n", rtnval, transferred);
+
+        nanosleep(&t, NULL);
+    }
+
+    printf("\n[*] Starting input read\n");
+
+    intr_interval = intr_in->bInterval;
+
+    printf("[*] bInterval: %hhd ", intr_interval);
+
+    if (speed >= LIBUSB_SPEED_HIGH)
+        interval_usec = (1 << (intr_interval - 1)) * 125;
+    else
+        interval_usec = intr_interval * 1000;
+
+    printf("(%d usec)\n", interval_usec);
+
+    t.tv_sec = 0;
+    t.tv_nsec = interval_usec * 1000;
+
+    while (1) {
+        memset(int_buffer, 0, INT_SIZE);
+        transferred = 0;
+
+        rtnval = libusb_interrupt_transfer(dev_handle, intr_in->bEndpointAddress, (uint8_t *) int_buffer, INT_SIZE, &transferred, 0);
+
+        for (i=0; i<4; i++) {
+            for (j=0; j<32; j++) {
+                printf( ( int_buffer[3 - i] >> (31 - j) ) & 1 ? "1" : "0" );
             }
-            printf("\n");
         }
-        printf("\n");
+        printf("\t%d %d   \r");
+	nanosleep(&t, NULL);
     }
 
     libusb_free_config_descriptor(config);
